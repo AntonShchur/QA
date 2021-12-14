52cc441dcb2da5f622e9adff0d871ade0fc79fc0
AntonShchur <54263356+AntonShchur@users.noreply.github.com>
2021-12-14 09:54:25 +0200

    Preconditions + trying page object patterb

--- LAB_5/test_Rozetka.py
+++ LAB_5/test_Rozetka.py
@@ -1,56 +1,40 @@
+from page_objects import *
 from unittest import TestCase
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-import allure
-
-class Test_Rozetka(TestCase):
-    def test_rozetka(self):
-        search_text = 'rtx 3080'
-        url = "https://rozetka.com.ua/ua/"
-        browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-        browser.implicitly_wait(10)
-
-        browser.get(url)
-        browser.find_element(by=By.CLASS_NAME, value='search-form__input').send_keys(search_text)
-        browser.find_element(by=By.CLASS_NAME, value='search-form__input').send_keys(Keys.ENTER)
-        response = browser.find_element(by=By.CLASS_NAME, value='goods-tile__title').text
-
-        assert search_text in response.lower()
-        browser.close()
+from selenium.webdriver.common.keys import Keys
 
 
-    def test_filter_rozetka(self):
-        filter_name = "Комп'ютери та ноутбуки"
-        url = "https://rozetka.com.ua/ua/"
-        browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-        browser.implicitly_wait(10)
-        browser.maximize_window()
+class Test_rozetka(TestCase):
 
-        browser.get(url)
-        browser.find_element(by=By.ID, value="fat-menu").click()
-        browser.implicitly_wait(10)
-        browser.find_element(by=By.LINK_TEXT, value="Ноутбуки та комп’ютери").click()
-        browser.implicitly_wait(10)
-        response = browser.find_element(by=By.CLASS_NAME, value="portal__heading").text
-        assert response == filter_name
-
-        browser.close()
-
+    def setUp(self) -> None:
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        self.driver.get("https://rozetka.com.ua/ua/")
+        self.driver.implicitly_wait(10)
+        self.driver.maximize_window()
 
     def test_search_button(self):
-        search_text = 'rtx 3080'
-        url = "https://rozetka.com.ua/ua/"
-        browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-        browser.maximize_window()
+        text_to_search = "rtx 3080"
+        main_page = SearchHelper(self.driver)
+        main_page.enter_text(text_to_search)
+        main_page.click_on_the_search_button()
+        assert text_to_search.lower() in main_page.find_text().lower()
 
-        browser.implicitly_wait(10)
-        browser.get(url)
-        browser.find_element(by=By.CLASS_NAME, value='search-form__input').send_keys(search_text)
-        browser.find_element(by=By.CLASS_NAME, value="search-form").find_element(by=By.CLASS_NAME, value="button").click()
-        response = browser.find_element(by=By.CLASS_NAME, value='goods-tile__title').text
-        assert search_text in response.lower()
+    def test_search_field(self):
+        text_to_search = "rtx 3070"
+        main_page = SearchHelper(self.driver)
+        main_page.enter_text(text_to_search)
+        main_page.enter_text(Keys.ENTER)
+        assert text_to_search.lower() in main_page.find_text().lower()
 
-        browser.close()
+    def test_goods_filter(self):
+        filter_name = "Комп'ютери та ноутбуки"
+        filter_menu = CategoryFilter(self.driver)
+        filter_menu.click_on_filter_menu()
+        filter_menu.click_on_pc_category()
+        response_category_name = filter_menu.find_text()
+        assert response_category_name.lower() == filter_name.lower()
+
+    def tearDown(self) -> None:
+        self.driver.close()
